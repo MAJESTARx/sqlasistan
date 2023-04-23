97e3e46d9f4d6d6b67b843a187558ca389e30771
Majestar <50384617+MAJESTARx@users.noreply.github.com>
2023-04-23 22:34:11 +0300

    Add files via upload

---
+++ benioku.md
@@ -0,0 +1,13 @@
+Kurulum için ; 
+
+Terminale 
+sudo bash gereksinimler.sh 
+ yazın ve kurulumun bitmesini bekleyin
+ardından 
+
+terminale  tırnak işareti olmadan " sqlasistan " 
+yazın /bin dizininde olduğu için otomatik çalışacaktır
+
+iyi kullanımlar
+
+# CREATED BY MAJESTAR 23.04.2023
---
+++ gereksinimler.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#!/bin/sh
+#Created By MAJESTAR	
+if [ "$EUID" -ne 0 ] 
+  then 
+  clear
+ echo -e "\033[32;5m
+ Lütfen  Sudo Kullanıcı olarak çalıştırın \033[0m"
+  sleep 1
+ echo -e "\033[1;31m  Şunu Yazın  : sudo bash gereksinimler.sh \033[4;34m"
+  exit
+fi 
+cp sqlasistan /bin 
+ls /bin | grep sqlmap >> /dev/null 
+if [ "$?" != 0 ];then
+apt-get install sqlmap
+fi 
+ls /usr/share/figlet | grep Bloody >> /dev/null
+if [ "$?" != 0 ];then
+apt-get install figlet -y 
+git clone https://github.com/xero/figlet-fonts
+cd figlet-fonts
+sudo chmod 777 * 
+mv * /usr/share/figlet
+cd .. 
+rm -rf figlet-fonts
+fi
+ls /usr/games | grep lolcat >> /dev/null
+if [ "$?" != 0 ];then
+sudo apt-get install lolcat -y
+bash sqlasistan
+fi
+echo""
+echo""
+
+echo "KURULUM BİTTİ TERMİNALE ( sqlasistan ) yazarak programı çalıştırabilirsiniz "
+
